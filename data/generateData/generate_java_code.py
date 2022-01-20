b3bb26b1c030e415b09fef6d6db0296f33bfb19b
unknown <nofexx@bk.ru>
2022-01-20 09:21:25 +0300

    add some one cheat

---
+++ data/generateData/generate_java_code.py
@@ -0,0 +1,20 @@
+class JavaCodeGeneration:
+    def __init__(self):
+        pass
+
+    def activist(self):
+        pass
+
+    def factory(self):
+        pass
+
+    def worker(self):
+        pass
+
+
+def main():
+    java_code = JavaCodeGeneration()
+
+
+if __name__ == '__main__':
+    main()
